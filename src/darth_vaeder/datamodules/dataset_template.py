5cf33cc0a92fdd7907f530568b27918d0e4e5ad1
DA <dandanmao66@gmail.com>
2026-06-14 15:51:54 +0000

    random draw for whole plate normalizatin

--- src/darth_vaeder/datamodules/dataset_template.py
+++ src/darth_vaeder/datamodules/dataset_template.py
@@ -7,24 +7,76 @@ import tifffile as tiff
 
 csv_path="/mnt/efs/dl_jrc/student_data/S-DA/image_metadata_BR00149208.csv"
 df=pd.read_csv(csv_path)
+
+# incrementing ID per field, 2000 ish in total 
 df["Group_id"] =  df.groupby(['Row', 'Column', 'Field']).ngroup()
+
+# incrementing ID per well, 60 in total 
+df["Well_id"] =  df.groupby(['Row', 'Column']).ngroup()
+
+# get unique well IDs 
+unique_well_IDs = df["Well_id"].unique()
+
+# percentage of fields to sample from each well 
+n_sample = 5 # as % 
+n = int((n_sample * len(unique_well_IDs)) / 100)
+
+
+
+fields_to_draw = []
+seed = 42
+rng = np.random.default_rng(seed)
+
+for well in unique_well_IDs:
+    mask = df["Well_id"] == well
+
+    well_df = df[mask]
+
+    unique_field_ids = well_df["Field"].unique()
+    fields = rng.choice(unique_field_ids, size = n, replace = False)
+    field_mask = well_df["Field"].isin(fields)
+    
+    unique_fields = well_df[field_mask]["Group_id"].unique()
+    fields_to_draw.append(unique_fields)
+
+
+fields_to_draw = np.concatenate(fields_to_draw)
+
+
+
+all_paths = []
+#TODO: we are currently ding this for field 0. Do this for all of our fields in fields_to_draw
+mask = (df["Group_id"] == fields_to_draw[0]) & (df["Channel Index"] == 1)
+
+all_paths.append(df[mask]["Path"].iloc[0])
+
+
+
+
+
+
+
+
+
+
+def get_norm_stats(df):
+
+
+
+
+
+
+
 
 
 class CustomImageDataset(Dataset):
     def __init__(
         self,
         metadata_file,
-        annotations_file,
     ):
         
-        self.df = df
-        self.img_labels = pd.read_csv(annotations_file)
         self.metadata_file=metadata_file
-   #     self.img_dir = img_dir
-   #     self.transform = transform
 
-        # get all relevant images and annotations and load them
-        # into memory
 
     def __len__(self):
         # Figure out the total number of samples in the dataset and return it
@@ -35,34 +87,27 @@ class CustomImageDataset(Dataset):
      # Center crop
      # load image
 
-        center_cropped_img = self.centercrop(...)
-        for channel in range(center_cropped_img.shape[]
-            self.compute_normalization(img)   
     
     # compute normalization 
     
 
-    def preprocess_normalization(self,img) ->dict:
-        #load image and normalization by robust Z
+    # def preprocess_normalization(self,img) ->dict:
+    #     #load image and normalization by robust Z
 
 
-        return{
-            "mean":...,
-            "median":...,
-            "std":...,
-            "iqr":...
-        }
-    
+    #     return{
+    #         "mean":...,
+    #         "median":...,
+    #         "std":...,
+    #         "iqr":...
+    #     }
+
+    #     # add the statistic columns ( mean median, etc)
 
 
-       
+    #     #save the new metadata file with stats
 
-        # add the statistic columns ( mean median, etc)
-
-
-        #save the new metadata file with stats
-
-        return 
+    #     return 
 
     
     def centercrop(self, img, sidelength = 256):
@@ -116,3 +161,11 @@ class CustomImageDataset(Dataset):
 
 getdataset = CustomImageDataset(df)
 
+
+
+
+# 
+
+        # center_cropped_img = self.centercrop(...)
+        # for channel in range(center_cropped_img.shape[]
+        #     self.compute_normalization(img)   
